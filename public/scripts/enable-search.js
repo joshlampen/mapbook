4638f9fe80356365d257bcf6336f197036a10be9
JoshLampen <joshlampen2@gmail.com>
2020-05-29 21:02:42 +0000

    Users are now able to search for locations on the map using a search bar; modularized function into separate js files

---
+++ public/scripts/enable-search.js
@@ -0,0 +1,50 @@
+const enableSearch = function(searchBox, map) {
+
+  // change map bounds upon location search
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+
+  let markers = [];
+
+  searchBox.addListener('places_changed', function() {
+    const places = searchBox.getPlaces();
+
+    if (places.length === 0) {
+      return;
+    }
+
+    // for each marker that is found from search...
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+
+    markers = [];
+
+    // get the current bounds of the map
+    const bounds = new google.maps.LatLngBounds();
+
+    // for each place that is found...
+    places.forEach(function(place) {
+      if (!place.geometry) {
+        return;
+      }
+
+      // create a new marker with relevant info
+      markers.push(new google.maps.Marker({
+        map: map,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      // change the map bounds to include the marker within its view
+      if (place.geometry.viewport) {
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+
+    map.fitBounds(bounds);
+  });
+};
--- public/scripts/load-map.js
+++ public/scripts/load-map.js
@@ -1,15 +1,54 @@
+// this script loads the initial empty map
+
 $(document).ready(function() {
-  let map;
+  let map, infoWindow;
 
   const createMap = function() {
+    // initial map load
     const options = {
       center: { lat: 43.654, lng: -79.383},
-      zoom: 10
+      zoom: 10,
+      // disableDefaultUI: true --> this would remove all the in-map features e.g. zoom
+      // draggable: false --> this would disable the ability to drag on the map
     };
 
     map = new google.maps.Map(document.getElementById('map'), options);
+
+    // load user position if possible, if not handle edge cases
+    infoWindow = new google.maps.InfoWindow;
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(p) {
+        let position = {
+          lat: p.coords.latitude,
+          lang: p.coords.longitude
+        };
+
+        infoWindow.setPosition(position);
+        infoWindow.setContent('Your location!');
+        infoWindow.open(map);
+
+      }, function() {
+        handleLocationError('Geolocation service failed', map.center());
+
+      });
+    } else {
+      handleLocationError('No geolocation available', map.center());
+
+    }
+
+    // load search bar
+    const input = document.getElementById('search');
+    const searchBox = new google.maps.places.SearchBox(input);
+
+    enableSearch(searchBox, map);
+  };
+
+  const handleLocationError = function(content, position) {
+    infoWindow.setPosition(position);
+    infoWindow.setContent(content);
+    infoWindow.open(map);
   };
 
   createMap();
 });
-
