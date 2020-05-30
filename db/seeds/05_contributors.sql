ccd5dfa0f26c25f76aa33121f76bd1041e54d334
JoshLampen <joshlampen2@gmail.com>
2020-05-30 18:49:51 +0000

    Changed entity name from contributors to contributions

--- db/seeds/05_contributors.sql
+++ db/seeds/05_contributors.sql
@@ -1,3 +1,3 @@
-INSERT INTO contributors (user_id, map_id) VALUES (1, 3);
-INSERT INTO contributors (user_id, map_id) VALUES (2, 1);
-INSERT INTO contributors (user_id, map_id) VALUES (3, 2);
+INSERT INTO contributions (user_id, map_id) VALUES (1, 3);
+INSERT INTO contributions (user_id, map_id) VALUES (2, 1);
+INSERT INTO contributions (user_id, map_id) VALUES (3, 2);
