1be12ef0bec575a24bf0266a0fcaf92853be985f
Aaron Timbol <timbolaj@gmail.com>
2020-05-31 14:18:12 +0000

    Created function to format marker info into geoJson which will allow us to show all the markers on a map. Still need to connect it to db and dynamically append it to the script

--- db/dbFunctions.js
+++ db/dbFunctions.js
@@ -1,5 +1,28 @@
 //db functions
 
+//Helper function - Take the array of lat and long values and convert to geoJson
+const toGeoJson = array => {
+  let geoJson = {
+    "type": "User destinations",
+    "features": []
+  }
+
+  array.forEach(queryObj => {
+    let featuresObject = {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": []
+      }
+    }
+    featuresObject.geometry.coordinates = [queryObj.latitude, queryObj.longitude];
+    toGeoJson.features.push(featuresObject);
+  })
+  return geoJson;
+};
+
+//Get markers and load to map
 const getMarkers = mapId => {
   const query = `
   SELECT latitude, longitude FROM markers
@@ -7,12 +30,12 @@ const getMarkers = mapId => {
   `
   return db.query(query, [mapId])
   .then(data => {
-    //Need to format info into GEOJson and use a google function
-    map.data.loadGeoJson(/* Query lat and long, but in GeoJSON form */)
+    //Formats into geoJson, but this needs to be appended dynamically as a script
+    map.data.addGeoJson(toGeoJson(data.rows))
   })
   .catch(err => {
     res
       .status(500)
       .json({ error: err.message });
   });
-}
+};
