5bf75bbb3fbf273bbe9c68d26f9ec9ec1c372d8a
Andrew Trac <andrewtrac87@hotmail.com>
2020-05-31 23:17:20 +0000

    linked login page to SQL database

--- routes/users.js
+++ routes/users.js
@@ -13,36 +13,26 @@ const router  = express.Router();
 
 module.exports = (db) => {
 
-  router.get("/", (req, res) => {
+  router.get('/', (req, res) => {
+    res.render('login')
+  })
 
-    res.render('login') // Calls the login page.
+  router.post("/", (req, res) => {
 
-    db.query(`SELECT * FROM users;`)  // THis is leftover from the original version I pulled. It has not been implememented.
-      .then(data => {
-        const users = data.rows;
-        res.json({ users });
+      const {email, password} = req.body;
+
+      const loginQuery = `
+      SELECT * FROM users
+      WHERE email = $1`
+
+      res.redirect('/api/users')
+
+      return db.query(loginQuery, [email]).then((res) => {
+        console.log(res.rows)
+         // Calls the login page.
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-  });
 
-  /*
-  //Will need to expand on this once we can register users
-  router.post("/users", (req, res) => {
-    const name = req.name
-    const password = req.password
-    const email = req.email
-    const query = `
-    INSERT INTO users (name, email, password)
-    VALUES ($1, $2, $3)
-    `
-    return db.query(query, [name, email, password])
-    .then(res => res.rows[0])
-    .catch(err => console.error("Error", err.stack));
-  }) */
+      })
 
   //Create route to get user info
 
