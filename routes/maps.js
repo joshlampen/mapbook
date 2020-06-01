7ef5b8c4d8eb9afec3a499adc025ee892cf16a13
JoshLampen <joshlampen2@gmail.com>
2020-06-01 19:28:30 +0000

    Fixed router

--- routes/maps.js
+++ routes/maps.js
@@ -41,52 +41,6 @@ module.exports = (db) => {
     DELETE FROM maps
     WHERE id = $1;
     `, values)
-<<<<<<< HEAD
-      .then(data => res.json(data.rows))
-      .catch(e => e);
-  })
-
-  router.post('/markers/delete', (req, res) => {
-    const markerID = req.body.markerID;
-    const mapID = req.body.mapID;
-
-    const values = [markerID, mapID];
-
-    return db.query(`
-    DELETE FROM markers
-    WHERE id = $1
-    AND map_id = $2;
-    `, values)
-  })
-
-  //Get all the favorited maps - Works! Will need to modify once we have all the ids
-  router.get('/favorites', (req, res) => {
-    const userId = req.session.user_id
-    const query = `
-    SELECT * FROM favorites
-    WHERE user_id = $1
-    `
-    return db.query(query, [userId])
-    .then(res => {
-      console.log(res)
-    })
-    .catch(err => console.log("Error", err.stack))
-
-  })
-
-  //Favorite a map -- Will develop this further once favorites functionality has been setup
-  router.post('/favorites', (req, res) => {
-    const userId = req.session.user_id
-    const query =`
-    INSERT INTO favorites (user_id, map_id)
-    VALUES ($1, $2)
-    `
-
-    return db.query(query, [userId, mapId])
-    .then()
-    .catch(err => console.error("Error", err.stack));
-=======
->>>>>>> origin
   })
 
   router.get('/:mapName', (req, res) => { // gets id based on name and user_id
