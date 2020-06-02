1875982ee918e7b9a198d8419b8b980a01c692bb
Andrew Trac <andrewtrac87@hotmail.com>
2020-06-02 16:51:30 +0000

    Added hover animation and changed colors on feed

--- public/scripts/maps-feed-helpers.js
+++ public/scripts/maps-feed-helpers.js
@@ -6,7 +6,10 @@ const loadMapsFeed = function() {
 const addMap = function(map) {
   const html = `
   <div class="map" id="map-${map.id}">
+  <div class="title-container">
     <h4>${map.name}</h4>
+    <h6>Insert City Here</h6>
+  </div>
     <button class='favorite-map' id="favorite-map-${map.id}"><i class="far fa-heart"></i></button>
   </div>
   `;
